bc7312961d76530ac8196359f3e3af86db25797f
Sysuev <KSysuev@luxoft.com>
2020-11-30 09:36:40 +0300

    Optimization

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,24 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: {
-    index: "./src/index.js",
     print: "./src/print.js",
+    index: {
+      import: "./src/index.js",
+      dependOn: "shared",
+    },
+    another: {
+      import: "./src/another-module.js",
+      dependOn: "shared",
+    },
+    shared: "lodash",
   },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
   module: {
     rules: [
